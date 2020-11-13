ec7d9c9c6bf25231cdc517ebc1a34b45a65199b1
Neil McCracken <50129064+mccrackenn@users.noreply.github.com>
2020-11-13 13:41:19 -0800

    initial car prototype

---
+++ CarGalleryApp/Controllers/CarsController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarGalleryApp.Models;
+using CarGalleryApp.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarGalleryApp.Controllers
+{
+    public class CarsController : Controller
+    {
+        private readonly CarService carService;
+        public CarsController(CarService carService)
+        {
+            this.carService = carService;
+        }
+        // GET: CarsController
+        public ActionResult Index()
+        {
+            return View(carService.Get());
+        }
+
+        // GET: CarsController/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var car = carService.Get(id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+            return View(car);
+        }
+
+        // GET: CarsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CarsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Car car)
+        {
+            if (ModelState.IsValid)
+            {
+                carService.Create(car);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(car);
+            //try
+            //{
+            //    return RedirectToAction(nameof(Index));
+            //}
+            //catch
+            //{
+            //    return View();
+            //}
+        }
+
+        // GET: CarsController/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var car = carService.Get(id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+            return View(car);
+        }
+
+        // POST: CarsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(string id, Car car)
+        {
+            if(id != car.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                carService.Update(id, car);
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(car);
+            }
+        }
+
+        // GET: CarsController/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var car = carService.Get(id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+            return View(car);
+        }
+
+        // POST: CarsController/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            try
+            {
+                var car = carService.Get(id);
+
+                if (car == null)
+                {
+                    return NotFound();
+                }
+
+                carService.Remove(car.Id);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CarGalleryApp/CustomAttributes/YearRangeAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarGalleryApp.CustomAttributes
+{
+    public class YearRangeAttribute:RangeAttribute
+    {
+        public YearRangeAttribute(): base(typeof(int), DateTime.Now.AddYears(-50).Year.ToString(), DateTime.Now.AddYears(1).Year.ToString())
+        {
+
+        }
+    }
+}
